1b99782fc0697924d0a9e2befdd23669ff2aaf78
Michael Schenk <michuschenk@gmail.com>
2020-05-01 18:01:30 +0200

    Refactor most of the code for better structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scipy.integrate import quad, fixed_quad
 from scipy.interpolate import interp1d
 from scipy.constants import c, e, m_p
 
-from integrators import hlp2, hlp2_parallel
+from integrators import hlp2_parallel
 
 
 class Resonator:
@@ -63,54 +63,84 @@ class ImpedanceFile:
         return z
 
 
-def avg_Q(r, R, eta, Qs, Qpp):
-    return Qpp * Qs ** 2 * r ** 2 / (4. * eta ** 2 * R ** 2)
+def update_machine_parameters(machine):
+    """ Calculates additional machine parameters and adds them to the dict.
+    :param beta: relativistic beta,
+    :param T0: revolution period [s]
+    :param eta: slippage factor
+    :param omega_0: revolution frequency [rad/s]
+    :param beta_z: longitudinal beta function [m]
+    :param radius: machine physical radius [m]
+    :param total_energy: total beam energy [J]
+    :param z_hat: airbag beam radius [m] """
+    beta = np.sqrt(1. - 1. / machine['gamma'] ** 2)
+    T0 = machine['circumference'] / (beta * c)
+    eta = machine['alpha_0'] - machine['gamma'] ** -2
+    omega_0 = 2. * np.pi / T0
+    beta_z = eta * T0 / machine['Qs']
+    radius = machine['circumference'] / (2. * np.pi)
+    total_energy = machine['gamma'] * machine['particle_mass'] * c ** 2
+    z_hat = np.sqrt(2. * beta_z * machine['Jz'])
+
+    machine.update(
+        {'beta': beta, 'T0': T0, 'eta': eta, 'omega_0': omega_0,
+         'beta_z': beta_z, 'radius': radius, 'total_energy': total_energy,
+         'z_hat': z_hat}
+    )
+
+def get_average_tuneshift(machine, Qpp):
+    """ Computes average tune shift for given airbag bunch and machine
+    parameters (see A. Maillard, Eq. 50).
+    :param machine: dictionary with main machine and beam parameters
+    :param Qpp: second-order chromaticity """
+    return ((Qpp * machine['Qs']**2 * machine['z_hat']**2) /
+            (4. * machine['eta']**2 * machine['radius']**2))
 
 
+def get_complex_tuneshift(machine, impedance, Qp=0., Qpp=0., l=0, p_max=120000):
+    """ Computes complex tune shift :param delta_Q for given machine and beam parameters
+    stored in :param machine_dict and for a specific impedance model :param impedance.
+    :param l denotes the azimuthal mode number, and :param p_max represents the
+    number of summands to consider (check convergence!). """
+    # TODO: automatic convergence check for p_max
 
-def get_deltaQ(Z=None, Jz=5e-4, Qp=0., Qpp=0., l=0, p_max=100000):
-    # Parameters (use something like SPS)
-    N = 1e9   # Number of protons in bunch
-
-    gamma = 27.7  # 103. #27.7 # Relativistic gamma, at injection 26 GeV. No units.
-    E0 = m_p * c**2 * gamma
-    beta = np.sqrt(1. - 1. / gamma**2)
-    C = 6911.  # [m], accelerator circumference
-    T0 = C / (beta * c)
-    R = C / (2. * np.pi)
-    omega0 = 1. / T0 * 2. * np.pi
-
-    alpha_0 = 0.00308
-    eta = alpha_0 - gamma ** -2  # Slippage factor
-    # eta = 0.001777 # Slippage factor, no units.
-    Q_beta = 20.18
-    omega_beta = Q_beta * omega0  # Betatron tune, here in y.
-    Qs = 0.017
-    omega_s = Qs * omega0  # Synchrotron tune, linear motion, Q20 optics.
-
-    beta_z = eta * C / (2. * np.pi) / (omega_s / omega0)
-    z_hat = np.sqrt(2. * beta_z * Jz)  # [m]
-
-    # Chromaticity
-    xi = Qp / (omega_beta / omega0)
-    chi = xi * omega_beta * z_hat / (c * eta)  # Head-tail phase
-
-    # CALCULATE TUNE SHIFT (Antoine Maillard, Eq. 50)
-    avQ = avg_Q(z_hat, R, eta, Qs, Qpp)
+    avg_Q = get_average_tuneshift(machine, Qpp)
     p_vect = np.arange(-p_max, p_max + 1)
-    omega_p = p_vect * omega0 + omega_beta + l * omega_s
-    Zeval = Z.evaluate(omega_p)
+    omega_p = ((p_vect + machine['Q_beta'] + l * machine['Qs']) *
+               machine['omega_0'])
+    imp_eval = impedance.evaluate(omega_p)
 
     # Serial versions of hlp2 very slow - hence use openMP parallel one
-    hlp2 = hlp2_parallel(Q0=Q_beta, Qs=Qs, rb=z_hat, R=R, eta=eta,
-                         Qp=Qp, Qpp=Qpp, l=l, p_max=p_max)
+    hlp2 = hlp2_parallel(
+        Q0=machine['Q_beta'], Qs=machine['Qs'], rb=machine['z_hat'],
+        R=machine['radius'], eta=machine['eta'],
+        Qp=Qp, Qpp=Qpp, l=l, p_max=p_max)
 
-    sum_term = np.sum(hlp2 * Zeval)
+    sum_term = np.sum(hlp2 * imp_eval)
 
-    delta_omega_l = -1j * N * e ** 2 * c / (2. * E0 * T0 ** 2 * omega_beta) * sum_term + avQ * omega0
-    return delta_omega_l / omega0
+    pre_factor = (
+        -1j * machine['number_particles'] * e ** 2 * c /
+        (2. * machine['total_energy'] * machine['T0'] ** 2 * machine['Q_beta']))
+    delta_Q = pre_factor * sum_term / machine['omega_0'] ** 2 + avg_Q
+
+    return delta_Q
 
 
+# Machine dictionary with main parameters
+SPS = {
+    'number_particles': 1e9,  # number of protons in bunch
+    'circumference': 6911.,  # [m]
+    'gamma': 27.7,  # relativistic gamma
+    'Qs': 0.017,  # synchrotron tune
+    'alpha_0': 0.00308,  # momentum compaction factor
+    'Q_beta': 20.18,  # betatron tune
+    'particle_mass': m_p,  # proton mass [kg]
+    'Jz': 3e-4  # longitudinal action [m]
+}
+update_machine_parameters(SPS)
+
+# Define impedance to be used.
+# Resonator parameters - peaked (i.e. narrow-band)
 # In PyHeadtail we do not use exactly the wake field definition
 # made by Chao.Hence, also Chao 's impedance formula should not be applied
 # here( in general), because of the different conventions. Basically,
@@ -118,6 +148,18 @@ def get_deltaQ(Z=None, Jz=5e-4, Qp=0., Qpp=0., l=0, p_max=100000):
 # compare the analytical formula with PyHeadtail simulations with the same
 # resonator parameters, i.e.especially same r_shunt and omega_res) we must
 # typically use 'PyHeadtail' convention.
+omega_res = 5.022163e8
+r_shunt = 1e6 * 5e6  # [Ohm/m**2].
+Q = 1e5
+resonator = Resonator(r_shunt, omega_res, Q, convention="PyHeadtail")
+
+# Compute tune shift for specific azimuthal mode
+Qp = 2
+azimuthal_mode = 1
+delta_Q = get_complex_tuneshift(SPS, resonator, Qp=Qp, Qpp=0., l=azimuthal_mode)
+print('delta_Q', delta_Q)
+
+"""
 
 # only scan in Q', to compare Antoine's Hlp function with Bessel from Chao.
 # in case of Q'' = 0, the two should be equivalent. And they are!
@@ -125,12 +167,7 @@ calc = True
 plot = True
 
 if calc:
-    # Define impedance to be used.
-    # Resonator parameters - peaked (i.e. narrow-band)
-    omegar = 5.022163e8
-    Rs = 1e6 * 5e6  # [Ohm/m**2].
-    Q = 1e5
-    Z = Resonator(Rs, omegar, Q, convention="PyHeadtail")
+
 
     # Dependence on Q'
     Qp_vect = np.linspace(-2, 2, 5)
@@ -144,8 +181,8 @@ if calc:
         for Qp in Qp_vect:
             if ctr % 5 == 0:
                 print('{:d} / {:d}'.format(ctr, len(Qp_vect) * len(l_vect)))
-            deltaQ_res.append(get_deltaQ(Z=Z, Jz=3e-4, Qp=Qp, Qpp=0., l=l,
-                                         p_max=120000))
+            deltaQ_res.append(get_complex_tuneshift(Z=Z, Jz=3e-4, Qp=Qp, Qpp=0., l=l,
+                                                    p_max=120000))
             ctr += 1
         deltaQ_re[i, :] = np.array(np.real(deltaQ_res))
         deltaQ_im[i, :] = np.array(np.imag(deltaQ_res))
@@ -186,4 +223,4 @@ if plot:
     plt.subplots_adjust(right=0.85, left=0.12)
     # plt.savefig('Antoine_analytic_RW_Jz3e-4_QpOnly.png', dpi=150)
     plt.show()
-
+"""
